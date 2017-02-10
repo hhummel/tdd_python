cf477597d0310b99c0ed777537fcb7f44fc0c07f
hhummel <harry.hummel@eventhorizoncapital.com>
2017-02-10 10:01:10 -0500

    Including capturing user input

--- functional_tests.py
+++ functional_tests.py
@@ -30,16 +30,28 @@ class NewVisitorTest(unittest.TestCase):
         #When she hits "Enter", the page updates and "1: Buy peacock feathers is an item on to-do list
         inputbox.send_keys(Keys.ENTER)
 
+        #import time
+        #time.sleep(10)
+
         table = self.browser.find_element_by_id("id_list_table")
         rows = self.browser.find_elements_by_tag_name("tr")
         self.assertTrue(any(row.text == "1: Buy peacock feathers" for row in rows), "New to-do item did not appear in table")
 
         #There's a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
-        self.fail("Finish the test!")
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)
+        
 
         #The page updates, and shows both items on her list
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = self.browser.find_elements_by_tag_name("tr")
+        self.assertIn("1: Buy peacock feathers", [row.text for row in rows], "First to-do item did not appear in table")
+        self.assertIn("2: Use peacock feathers to tie a fly", [row.text for row in rows], "Second to-do item did not appear in table")
+        
 
-        #She sees site has gnerated a unique URL for her list
+        #She sees site has generated a unique URL for her list
+        self.fail('Finish the test!')
 
         #She visits the URL and sees her list is present
 
