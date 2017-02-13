1fef9c1d41cb89728a5146682b32609ff0764f16
hhummel <harry.hummel@eventhorizoncapital.com>
2017-02-13 17:18:38 -0500

    New URL, view and template to display list

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -7,9 +7,10 @@ class NewVisitorTest(LiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Firefox()
-        self.browser.implicitly_wait(3)
+        self.browser.implicitly_wait(5)
 
     def tearDown(self):
+        time.sleep(10)
         self.browser.quit()
 
     def check_for_row_in_list_table(self, row_text):
@@ -37,15 +38,16 @@ class NewVisitorTest(LiveServerTestCase):
         #When she hits "Enter", she is sent to a new URL
         #and now the page lists "1: Buy peacock feathers is an item on to-do list
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
         edith_list_url = self.browser.current_url
-        self.assertRegex(edith_list_url, '/lists/.+')        
+        self.assertRegex(edith_list_url, '/lists/.+', "edith_list_url: " + edith_list_url)        
         self.check_for_row_in_list_table("1: Buy peacock feathers")
 
         #There's a text box inviting her to add another item. She enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Use peacock feathers to make a fly")
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(3)
 
         #The page updates, and shows both items on her list
         self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
@@ -63,6 +65,7 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys("Buy milk")
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         #Francis gets her own URL
         francis_list_url = self.browser.current_url
